fa849d40f63bdaf8303adb2d1496792a40ffa405
Merrick Sapsford <merrick@sapsford.tech>
2018-02-13 16:19:14 +0100

    Add TabmanBar class

---
+++ Sources/Tabman/TabmanBar/TabmanBar.swift
@@ -0,0 +1,13 @@
+//
+//  TabmanBar.swift
+//  Tabman
+//
+//  Created by Merrick Sapsford on 13/02/2018.
+//  Copyright © 2018 UI At Six. All rights reserved.
+//
+
+import UIKit
+
+open class TabmanBar: UIView {
+    
+}
